865862b95eb9ce934195dee4388284ed5c2cedeb
Adithyarao19 <102149755+Adithyarao19@users.noreply.github.com>
2024-10-15 23:04:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🤖 ML app')
 
 st.info('This app builds ML model!')
+df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
