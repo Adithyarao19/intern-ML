0712ee88c9fb2ac59dbb26337490e2a191c90cec
Adithyarao19 <102149755+Adithyarao19@users.noreply.github.com>
2024-10-16 16:11:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,4 @@ st.info('This app builds ML model!')
 with st.expander('Data'):
   st.write('Raw data**')
   df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv')
+df
